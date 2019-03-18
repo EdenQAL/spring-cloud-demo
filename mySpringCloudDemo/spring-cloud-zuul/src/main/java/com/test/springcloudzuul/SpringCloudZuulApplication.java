098d5a18bf066895c6ba32ea582e975e52bd2772
qiailei <1107058011@qq.com>
2019-03-18 16:25:39 +0800

    hystrics-dashboard整合完成节点备份

--- mySpringCloudDemo/spring-cloud-zuul/src/main/java/com/test/springcloudzuul/SpringCloudZuulApplication.java
+++ mySpringCloudDemo/spring-cloud-zuul/src/main/java/com/test/springcloudzuul/SpringCloudZuulApplication.java
@@ -3,7 +3,12 @@ package com.test.springcloudzuul;
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableHystrix
+@EnableHystrixDashboard
 
 @SpringCloudApplication
 @EnableZuulProxy
