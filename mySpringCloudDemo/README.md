1115289dea1fb9b3c88c8bcd5138ab9a7339f881
qiailei <1107058011@qq.com>
2019-03-18 17:22:15 +0800

    集群监控配置完成

--- mySpringCloudDemo/README.md
+++ mySpringCloudDemo/README.md
@@ -2,10 +2,9 @@
 master版：包含基本的springcloud框架,包括springcloud config注册中心
 、eureka服务注册中心、zuul网关中心的实现；
 
-basic-20190313： 包括feign功能实现及其他的实现;
+basic-20190313： 完成feign整合，ribbon由于在feign中已存在，此处不再单独整合; 完成hystrics-dashboard的整合，验证完成
 
-
-
+20190313：整合集群监控turbine；整合Zipkin
 
 各模块端口：
 
--- mySpringCloudDemo/spring-cloud-zuul/src/main/java/com/test/springcloudzuul/SpringCloudZuulApplication.java
+++ mySpringCloudDemo/spring-cloud-zuul/src/main/java/com/test/springcloudzuul/SpringCloudZuulApplication.java
@@ -5,10 +5,12 @@ import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @EnableHystrix
 @EnableHystrixDashboard
+@EnableTurbine
 
 @SpringCloudApplication
 @EnableZuulProxy
